35041e120ce029923c9f6f0c53c4e105166de6da
Masseh ziaie <mas@massehs-laptop.mynetworksettings.com>
2024-12-26 03:38:25 -0500

    added basic styles to result component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Result from "./Result";
 import Summary from "./Summary";
 
-const articleStyles = "w-1/2";
+const articleStyles = "w-1/2 text-lg p-11";
 
 export default function App() {
   return (
     <main className="h-screen bg-neutral-Pale-blue flex justify-center items-center">
-      <div className="bg-neutral-White w-full max-w-[736px] flex p-3">
+      <div className="bg-neutral-White w-full max-w-[736px] flex rounded-[32px]">
         <Result styles={articleStyles} />
         <Summary styles={articleStyles} />
       </div>
--- src/Result.tsx
+++ src/Result.tsx
@@ -3,5 +3,19 @@ type StyleProp = {
 };
 
 export default function Result({ styles }: StyleProp) {
-  return <article className={styles}>Hello</article>;
+  return (
+    <article className={`${styles} text-center`}>
+      <h2 className="text-2xl font-bold mb-8">Your Result</h2>
+      <div className="font-bold bg-primary-light-red rounded-full aspect-square w-[200px] flex items-center justify-center flex-col mx-auto">
+        <p>
+          <span className="text-7xl font-extrabold">76</span>
+          <div>of 100</div>
+        </p>
+      </div>
+      <h3 className="text-2xl  mt-7 mb-3.5 font-bold">Great</h3>
+      <p className="font-medium">
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </article>
+  );
 }
