d66f12d253404d84802c6fe9867bf6009986c5e8
Masseh ziaie <mas@massehs-laptop.mynetworksettings.com>
2024-12-27 02:33:18 -0500

    Finished the result component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const articleStyles = "w-1/2 text-lg p-11";
 export default function App() {
   return (
     <main className="h-screen bg-neutral-Pale-blue flex justify-center items-center">
-      <div className="bg-neutral-White w-full max-w-[736px] flex rounded-[32px]">
+      <div className="bg-neutral-White w-full max-w-[736px] flex rounded-[32px] overflow-hidden">
         <Result styles={articleStyles} />
         <Summary styles={articleStyles} />
       </div>
--- src/Result.tsx
+++ src/Result.tsx
@@ -4,16 +4,20 @@ type StyleProp = {
 
 export default function Result({ styles }: StyleProp) {
   return (
-    <article className={`${styles} text-center`}>
-      <h2 className="text-2xl font-bold mb-8">Your Result</h2>
-      <div className="font-bold bg-primary-light-red rounded-full aspect-square w-[200px] flex items-center justify-center flex-col mx-auto">
-        <p>
+    <article
+      className={`${styles} text-center bg-gradient-to-b from-Light-slate-blue  to-Light-royal-blue text-neutral-White`}
+    >
+      <h2 className="text-2xl font-bold mb-8 text-neutral-White text-opacity-70">
+        Your Result
+      </h2>
+      <div className="font-bold  rounded-full aspect-square w-[200px] flex items-center justify-center flex-col mx-auto bg-gradient-to-b from-Violet-blue to-Persian-blue">
+        <p className="flex flex-col">
           <span className="text-7xl font-extrabold">76</span>
-          <div>of 100</div>
+          <span className="text-neutral-White text-opacity-70">of 100</span>
         </p>
       </div>
       <h3 className="text-2xl  mt-7 mb-3.5 font-bold">Great</h3>
-      <p className="font-medium">
+      <p className="font-medium text-neutral-White opacity-70">
         You scored higher than 65% of the people who have taken these tests.
       </p>
     </article>
