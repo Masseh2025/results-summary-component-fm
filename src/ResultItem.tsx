26996de2ad9ce8ea9b38eec7a36decab3b1943c0
Masseh ziaie <mas@massehs-laptop.mynetworksettings.com>
2024-12-27 17:08:17 -0500

    I added list item component for the summary

---
+++ src/ResultItem.tsx
@@ -0,0 +1,30 @@
+type ReactionProp = {
+  resultName: string;
+  score: number;
+  color: string;
+  image: string;
+};
+
+export default function ReactionItem({
+  resultName,
+  score,
+  color,
+  image,
+}: ReactionProp) {
+  return (
+    <li
+      className={`bg-${color} bg-opacity-10 p-4 rounded-xl flex justify-between`}
+    >
+      <div className="flex">
+        <img src={image} alt="" />
+        <p className={`text-${color} ml-2`}>{resultName}</p>
+      </div>
+      <p className="text-neutral-Dark-gray-blue font-bold">
+        {score}{" "}
+        <span className="text-neutral-Dark-gray-blue text-opacity-50">
+          / 100
+        </span>
+      </p>
+    </li>
+  );
+}
--- src/Summary.tsx
+++ src/Summary.tsx
@@ -1,7 +1,27 @@
+import ResultItem from "./ResultItem";
+import data from "./data.json";
+
 type StyleProp = {
   styles: string;
 };
 
+import memory from "./assets/images/icon-memory.svg";
+import reaction from "./assets/images/icon-reaction.svg";
+import verbal from "./assets/images/icon-verbal.svg";
+import visual from "./assets/images/icon-visual.svg";
+
 export default function Summary({ styles }: StyleProp) {
-  return <article className={styles}>Hello</article>;
+  return (
+    <article className={styles}>
+      <h2>Summary</h2>
+      <ul>
+        <ResultItem
+          resultName="Reaction"
+          image={reaction}
+          color="primary-light-red"
+          score={80}
+        />
+      </ul>
+    </article>
+  );
 }
